a4388c702e0a7c9838733d53c4b7976f5b2cf29c
Yunke Wang <wangcoc2@gmail.com>
2015-03-30 22:19:52 -0400

    update linkedinviewer for multiple company updates

--- linkedinviewer.py
+++ linkedinviewer.py
@@ -10,11 +10,8 @@ class Linkedinviewer (object):
 
 	def authenticate(self):
 
-		# Define CONSUMER_KEY, CONSUMER_SECRET,  
-		# USER_TOKEN, and USER_SECRET from the credentials 
-		# provided in your LinkedIn application
-
-		cred_list = []
+		# Authenticate with LinkedIn app credential
+		cred_list = None
 		with open(self.cred_file, 'r') as f:
 			cred_data = f.readlines()
 			for line in cred_data:
@@ -22,6 +19,9 @@ class Linkedinviewer (object):
 					cred_temp = line.split('=')[1]
 				except:
 					print "Bad credentials for LinkedIn api authentication"
+				
+				if cred_list is None:
+					cred_list = []
 				cred_list.append(cred_temp.strip(' \t\n\r'))
 
 		# Authenticating with LinkedIn
@@ -33,6 +33,7 @@ class Linkedinviewer (object):
 		except:
 			print "Failed to authenticate with LinkedIn"
 
+		return None
 
 	def get_profile(self):
 		
@@ -42,7 +43,7 @@ class Linkedinviewer (object):
 
 		return profile
 
-	def get_company(self, company_id=None, universal_names=[]):
+	def get_company(self, company_id=None, universal_names=None):
 
 		# Get company information
 		companies = self.application.get_companies(company_id, universal_names)
@@ -50,21 +51,34 @@ class Linkedinviewer (object):
 
 		return companies
 
-	def get_company_updates(self, company_id=None, count=1):
+	def get_company_updates(self, companies=None, count=1):
 
 		# Get company updates
-		company_updates = self.application.get_company_updates(company_id, params={'count': count})
-		print company_updates
+		company_list = None
+		company_updates_dict = None
+		
+		print 'test'
+		if companies is not None:
+			for i in range(companies['_total']):
+				if company_list is None:
+					company_list = []
+				company_list.append(companies['values'][i])
+			for company in company_list:
+				if company_updates_dict is None:
+					company_updates_dict = {}
+				company_updates_dict[company['name']] = self.application.get_company_updates(company['id'], params={'count': count})
+			for company_name, company_updates in company_updates_dict.iteritems():
+				print '\n************************', company_name, '************************\n'
+				for i in range(company_updates['_count']):
+					print '========================\n'
+					print company_updates['values'][i]
+					print '\n========================'
 
-		return company_updates
+		return company_updates_dict
 
 if __name__ == "__main__":
 	lviewer = Linkedinviewer('linkedincred.conf')
 	lviewer.authenticate()
 	lviewer.get_profile()
-	companies = lviewer.get_company(universal_names=['splunk'])
-	company_list = []
-	for i in range(companies['_total']):
-		company_list.append(companies['values'][i])
-	for company in company_list:
-		lviewer.get_company_updates(company_id=company['id'], count=5)
+	companies = lviewer.get_company(universal_names=['apple', 'splunk'])
+	company_updates_dict = lviewer.get_company_updates(companies=companies, count=3)
