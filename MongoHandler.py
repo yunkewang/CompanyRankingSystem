25b16b2c45d6644802f8dc7a94cb609c0a105eae
Yunke Wang <yunke.w@gmail.com>
2015-05-12 01:52:22 -0400

    update mongohandler for crunchbase profile parsing

--- MongoHandler.py
+++ MongoHandler.py
@@ -1,8 +1,6 @@
 from pymongo import MongoClient
-import csv
-import json
+import csv, json, datetime, ast
 from bson.json_util import dumps
-import ast
 
 class MongoHandler(object):
 
@@ -72,12 +70,42 @@ class MongoHandler(object):
             print "Saving csv file failed"
 
 
+    def timestamp_convert(self, timestamp=None):
+
+        # Convert timestamp into year, month, and day
+        if timestamp is None:
+            return None
+        else:
+            timestamp_int = int(timestamp)
+            time_result = {}
+            time_result['year'] = datetime.datetime.fromtimestamp(timestamp_int).strftime('%Y')
+            time_result['month'] = datetime.datetime.fromtimestamp(timestamp_int).strftime('%m')
+            time_result['day'] = datetime.datetime.fromtimestamp(timestamp_int).strftime('%d')
+            return time_result
+
+    def l_profile_parser(self, linkedin_profile=None):
+        if linkedin_profile is None:
+            return None
+        else:
+            pass
+
+
+    def c_profile_parser(self, crunchbase_profile):
+        if crunchbase_profile = None:
+            return None
+        else:
+            crunchbase_profile_relationship = crunchbase_profile['relationships']
+            crunchbase_profile_headquarter = crunchbase_profile_relationship['headquarters']['items'][0]
+
+
     def cleanup(self):
 
         # Clean up data from database and insert into csv
         db_name = raw_input('Input name of db: ')
         collection_name = raw_input('Input name of collection: ')
         json_filename = raw_input('Input name of json file: ')
+        csv_filename = raw_input('Input name of csv file: ')
+
         if db_name is not None and collection_name is not None and json_filename is not None:
             db = self.mongo_client[db_name]
             try:
@@ -94,19 +122,31 @@ class MongoHandler(object):
             f_json.close()
         except:
             print "Saving json file failed"
+
+        try:
+            f_json = open(json_filename, 'r')
+            data_raw_json = json.load(f_json)
+            f_json.close()
+            data_selected_json = []
+            for data_raw in data_raw_json:
+                data_temp = {}
+                for field_name in ['name', 'location_city', 'location_country_code', 
+                                   'location_region', 'primary_role', 'short_description']:
+                    data_temp[field_name] = data_raw[field_name]
+                data_temp.update(self.c_profile_parser(data_raw['crunchbase_profile']))
+                data_temp.update(self.l_profile_parser(data_raw['linkedin_profile']))
+
+            f_csv = open(csv_filename, 'wb+')
+            writer_csv = csv.writer(f_csv)
+            writer_csv.writerow(data_json[0].keys())
+            for row in data_json:
+                try:
+                    writer_csv.writerow(row.values())
+                except:
+                    pass
+        except:
+            print "Saving csv file failed"
         
-        # try:
-        #     f_json = open(json_filename, 'r')
-        #     data_json = json.load(f_json)
-        #     f_json.close()
-        #     f_csv = open(csv_filename, 'wb+')
-        #     writer_csv = csv.writer(f_csv)
-        #     writer_csv.writerow(data_json[0].keys())
-        #     for row in data_json:
-        #         try:
-        #             writer_csv.writerow(row.values())
-        #         except:
-        #             pass
 
         return None
 
